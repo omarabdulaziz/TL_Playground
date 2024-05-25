069c59545626141cae103ff638b61ab8bdcae887
Omar Essam <omar12694@gmail.com>
2024-05-26 00:58:30 +0300

    code generation with FCN_param

---
+++ poscontrol/TLProj/controller/tl_basetypes.h
@@ -0,0 +1,39 @@
+/**************************************************************************************************\
+ *** 
+ *** Simulink model       : poscontrol
+ *** TargetLink subsystem : poscontrol/controller
+ *** Codefile             : tl_basetypes.h
+ ***
+ *** Generation date: 2024-05-26 00:55:58
+ ***
+ *** TargetLink version      : 5.2p1 from 23-Mar-2022
+ *** Code generator version  : Build Id 5.2.0.27 from 2022-03-21 17:27:57
+\**************************************************************************************************/
+
+#ifndef TL_BASETYPES_H
+#define TL_BASETYPES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef unsigned char Bool; /* Description: boolean basetype */
+typedef float Float32; /* Description: 32 bit floating-point basetype */
+typedef double Float64; /* Description: 64 bit floating-point basetype */
+typedef signed short int Int16; /* Description: 16 bit signed integer basetype */
+typedef signed long int Int32; /* Description: 32 bit signed integer basetype */
+typedef signed long long int Int64; /* Description: 64 bit signed integer basetype */
+typedef signed char Int8; /* Description: 8 bit signed integer basetype */
+typedef unsigned short int UInt16; /* Description: 16 bit unsigned integer basetype */
+typedef unsigned long int UInt32; /* Description: 32 bit unsigned integer basetype */
+typedef unsigned long long int UInt64; /* Description: 64 bit unsigned integer basetype */
+typedef unsigned char UInt8; /* Description: 8 bit unsigned integer basetype */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TL_BASETYPES_H */
+/*------------------------------------------------------------------------------------------------*\
+  END OF FILE
+\*------------------------------------------------------------------------------------------------*/
---
+++ poscontrol/TLProj/controller/tl_defines_Ctrl.h
@@ -0,0 +1,39 @@
+/**************************************************************************************************\
+ *** 
+ *** Simulink model       : poscontrol
+ *** TargetLink subsystem : poscontrol/controller
+ *** Codefile             : tl_defines_Ctrl.h
+ ***
+ *** Generation date: 2024-05-26 00:55:58
+ ***
+ *** TargetLink version      : 5.2p1 from 23-Mar-2022
+ *** Code generator version  : Build Id 5.2.0.27 from 2022-03-21 17:27:57
+\**************************************************************************************************/
+
+#ifndef TL_DEFINES_CTRL_H
+#define TL_DEFINES_CTRL_H
+
+#ifdef TL_FRAME
+#include "controller_frm.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef TL_FRAME
+
+#ifndef LOG_VAR
+#define LOG_VAR(param_0, param_1, param_2)
+#endif /* LOG_VAR */
+
+#endif /* TL_FRAME */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TL_DEFINES_CTRL_H */
+/*------------------------------------------------------------------------------------------------*\
+  END OF FILE
+\*------------------------------------------------------------------------------------------------*/
